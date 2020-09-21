ed93274519713c46975abd49fc2f79624b93dc4e
Daniel Destefanis <destefanis.drdestefanis@gmail.com>
2020-09-20 21:58:32 -0700

    Updated site

--- next-portfolio/components/hero.js
+++ next-portfolio/components/hero.js
@@ -6,28 +6,30 @@ function Hero() {
   return (
     <div className="grid">
       <div className={styles.hero}>
-        <motion.h1 
-          className={styles.title}
-          initial={{ opacity: 0, y: 60 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ type: 'spring', delay: 0, damping: 12 }}
-        >
-          I’m a designer who’s passionate about solving problems and creating delightful user experiences.
-        </motion.h1>
+        <div className={styles.titleWrapper}>
+          <motion.h1 
+            className={styles.title}
+            initial={{ opacity: 1, y: 200 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ type: 'spring', delay: 0.2, damping: 15 }}
+          >
+            I’m a designer who’s passionate about solving problems and creating delightful user experiences.
+          </motion.h1>
+        </div>
         <div className={styles.details}>
           <div className={styles.detail}>
             <motion.div
               initial={{ opacity: 0, scale: 0.6, y: 0 }}
               animate={{ opacity: 1, scale: 1, y: 0 }}
-              transition={{ type: 'spring', delay: 0.4, damping: 15 }}
+              transition={{ type: 'spring', delay: 0.4, damping: 12 }}
               className={styles.iconWrapper}>
                 <img src="/design-icon.svg" alt="Design Layout Icon" className={styles.detailIcon} />
             </motion.div>
             <motion.h3 
               className={styles.label}
-              initial={{ opacity: 0, y: 10 }}
+              initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ type: 'spring', delay: 0.65, damping: 15 }}>
+              transition={{ type: 'spring', delay: 0.65, damping: 12 }}>
               Design Manager at Discord
             </motion.h3>
           </div>
@@ -40,7 +42,7 @@ function Hero() {
               <img src="/map-icon.svg" alt="Map Icon" className={styles.detailIcon} />
             </motion.div>
             <motion.h3 
-              initial={{ opacity: 0, y: 10 }}
+              initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ type: 'spring', delay: 1, damping: 15 }}
               className={styles.label}>San Francisco
