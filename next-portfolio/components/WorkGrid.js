66c896f5d0ec65a007574c23f2e987e5de09b268
Daniel Destefanis <destefanis.drdestefanis@gmail.com>
2020-09-20 21:26:17 -0700

    Updated site with new component for cards, added animation on hover

--- next-portfolio/components/WorkGrid.js
+++ next-portfolio/components/WorkGrid.js
@@ -1,48 +1,52 @@
-import { motion } from 'framer-motion'
-
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 import styles from './WorkGrid.module.css'
+import Card from '../components/Card'
+import FeaturedCard from './FeaturedCard'
 
 function WorkGrid() {
   return (
     <div className="grid">
-      <div
-        className={styles.workGrid}
-        initial={{ opacity: 0, y: 60 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ type: 'spring', delay: 0.5 }}>
-        <div className={`${styles.card} ${styles.featured}`}>
-          <div className={styles.imageWrapper}>
-            <div className={styles.carousel}>
-              <img src="https://newportfolio.s3-us-west-2.amazonaws.com/server-video-image.png" alt="Discord server video ui" className={styles.image} />
-            </div>
-          </div>
+      <div className={styles.workGrid}>
+        {/* <div className={`${styles.card} ${styles.featured}`}>
+          <img src="https://newportfolio.s3-us-west-2.amazonaws.com/server-video-image.png" alt="Discord server video ui" className={styles.image} />
           <h6 className={styles.title}>Server Video</h6>
-        </div>
-        <div className={`${styles.card} ${styles.cardBlue}`}>
-          <div className={styles.imageWrapper}>
-            <img src="https://newportfolio.s3-us-west-2.amazonaws.com/discord-onboarding-image.png" alt="Discord Onboarding UI" className={styles.image} />
-          </div>
-          <h6 className={styles.title}>Discord Onboarding</h6>
-        </div>
-        <div className={`${styles.card} ${styles.cardGreen}`}>
-          <div className={styles.imageWrapper}>
-            <img src="https://newportfolio.s3-us-west-2.amazonaws.com/design-tooling-image.png" alt="Image of the design linting plugin" className={styles.image} />
-          </div>
-          <h6 className={styles.title}>Design Tooling</h6>
-        </div>
-        <div className={`${styles.card} ${styles.cardPurple}`}>
-          <div className={styles.imageWrapper}>
-            <img src="https://newportfolio.s3-us-west-2.amazonaws.com/destiny-medals-image.png" alt="Image of Destiny Medals Project" className={styles.image} />
-          </div>
-          <h6 className={styles.title}>Destiny Medals</h6>
-        </div>
-        <div className={`${styles.card} ${styles.cardPink}`}>
-          <div className={styles.imageWrapper}>
-            <img src="https://newportfolio.s3-us-west-2.amazonaws.com/steelseries-image.png" alt="SteelSeries Engine UI" className={styles.image} />
-          </div>
-          <h6 className={styles.title}>SteelSeries</h6>
-        </div>
+        </div> */}
+        <FeaturedCard 
+          image="https://newportfolio.s3-us-west-2.amazonaws.com/server-video-image.png"
+          imageAlt="Discord server video ui"
+          color="pink"
+          title="Server Video"
+          size="featured"
+        />
+        <Card 
+          image="https://newportfolio.s3-us-west-2.amazonaws.com/discord-onboarding-image.png"
+          imageAlt="Image of Destiny Medals Project"
+          color="blue"
+          title="Discord Onboarding"
+          size="normal"
+        />
+        <Card 
+          image="https://newportfolio.s3-us-west-2.amazonaws.com/design-tooling-image.png"
+          imageAlt="UI of Design Tooling Plugins in Figma"
+          color="green"
+          title="Design Tooling"
+          size="normal"
+        />
+        <Card 
+          image="https://newportfolio.s3-us-west-2.amazonaws.com/destiny-medals-image.png"
+          imageAlt="Image of Destiny Medals Project"
+          title="Destiny Medals"
+          color="purple"
+          size="normal"
+        />
+        <Card 
+          image="https://newportfolio.s3-us-west-2.amazonaws.com/steelseries-image.png"
+          imageAlt="SteelSeries Engine UI"
+          title="SteelSeries"
+          color="pink"
+          size="normal"
+        />
       </div>
     </div>
   )
